7a4f725b773ddaaf5e173596fab724331a2d6372
CodeConnects <codeconnects@gmail.com>
2023-07-20 10:01:12 -0400

    add V8 engine info to notes

---
+++ notes/notes.js
@@ -0,0 +1,13 @@
+/*
+Javascript engines
+*/
+
+// V8 compiles JavaScript directly to native machine code that your computer can execute
+
+// V8 engine was first created in 2008 by Google for use in Chrome
+// Google was having trouble with JavaScript performance for Google Maps and Gmail
+
+// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine
+
+/* -------------------------------------------------------------------------- */
+
