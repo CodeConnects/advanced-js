ed00339a0af66c8e7a55bf4d130baa1b8dd46747
CodeConnects <codeconnects@gmail.com>
2023-07-21 08:21:50 -0400

    add info about JIT compilers

--- notes/notes.js
+++ notes/notes.js
@@ -1,27 +1,28 @@
 /*
 ** JAVASCRIPT ENGINES **
 
-The first engine was created by Brendan Eich in 1995 for Netscape Navigator
-It was called SpiderMonkey, and is still used in Firefox today
+  The first engine was created by Brendan Eich in 1995 for Netscape Navigator
+  It was called SpiderMonkey, and is still used in Firefox today
+  SpiderMonkey is written in C++ and was originally interpreted
+  Later, SpiderMonkey was the first engine to use JIT (Just In Time) compilation
 
-V8 compiles JavaScript directly to native machine code that your computer can execute
+  V8 compiles JavaScript directly to native machine code that your computer can execute
 
-V8 engine was created in 2008 by Google for use in Chrome
-Google was having trouble with JavaScript performance for Google Maps and Gmail
+  V8 engine was created in 2008 by Google for use in Chrome
+  Google was having trouble with JavaScript performance for Google Maps and Gmail
 
-Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine
+  Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine
 
-*/
+--------------------------------------------------------------------------
 
-/* -------------------------------------------------------------------------- */
-
-/*
 ** JAVASCRIPT COMPILERS **
 
-Babel is a compiler that takes ultra modern JavaScript and compiles it down to JavaScript that works in any browser
+  Babel is a compiler that takes ultra modern JavaScript and compiles it down to JavaScript that works in any browser
 
-TypeScript is a superset of JavaScript that compiles down to plain JavaScript
+  TypeScript is a superset of JavaScript that compiles down to plain JavaScript
+
+  V8 is a JIT Compiler (Just In Time Compiler), compiles code at runtime
+
+-------------------------------------------------------------------------- 
 
 */
-
-/* -------------------------------------------------------------------------- */
