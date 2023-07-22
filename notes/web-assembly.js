905a8eca3a14474b3d684086f3dd62add172eac4
CodeConnects <codeconnects@gmail.com>
2023-07-22 08:38:02 -0400

    add info about Web Assembly

---
+++ notes/web-assembly.js
@@ -0,0 +1,14 @@
+/*
+** WEB ASSEMBLY
+
+  A standard binary executable format for the web
+
+  WebAssembly is a low-level assembly-like language with a compact binary format
+  It runs with near-native performance
+  It provides languages such as C/C++ and Rust with a compilation target so that they can run on the web
+  It is also designed to run alongside JavaScript, allowing both to work together
+
+  Could be a game-changer in the future as it removes the entire interpreter/compiler setup
+  Allows developers to write code in other languages and compile it to WebAssembly
+
+*/
