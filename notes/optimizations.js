a48afa07cd7bfeed8d55c654a6124585f075e277
CodeConnects <codeconnects@gmail.com>
2023-07-22 08:12:55 -0400

    add rules to optimize for Hidden Classes

---
+++ notes/optimizations.js
@@ -0,0 +1,20 @@
+/* HIDDEN CLASSES
+  1. Always put the same properties on an object, *IN THE SAME ORDER*
+  2. Use null rather than undefined
+  3. Avoid adding properties to an object after it's created
+  4. Use integers whenever possible
+  5. Don't delete properties from an object
+  6. Don't enumerate all properties on an object
+  7. Don't use __proto__
+  8. Use arrays as arrays and objects as objects
+  9. Avoid using the eval() function
+  10. Avoid using with()
+  11. Avoid using try/catch
+  12. Avoid declaring variables inside loops
+  13. Avoid deep nesting
+  14. Avoid using functions inside loops
+  15. Avoid using recursion
+  16. Avoid using bind()
+  17. Avoid using apply() and call()
+  18. Avoid using the for in loop
+*/
