875380c025764fb12b9fac9ae14c490a087cffe8
CodeConnects <codeconnects@gmail.com>
2023-07-24 09:08:32 -0400

    add info on functions passed no arguments

--- notes/arguments.js
+++ notes/arguments.js
@@ -11,4 +11,7 @@ Another way is using the spread operator: [...args]
 This is the preferred way, as it is more readable and concise.
 It makes a copy of the arguments with the spread operator, and then converts them to an array.
 
+Arguments exist as an empty object even if no arguments are passed to the function.
+In the console this appears as {} instead of undefined.
+
 */
