73754eab2a953625963df8e6f9a4768a5cd7606f
CodeConnects <codeconnects@gmail.com>
2023-07-24 08:55:52 -0400

    add spread operator method of arguments to array conversion

--- notes/arguments.js
+++ notes/arguments.js
@@ -5,6 +5,10 @@ Arguments are not actually arrays, but only array-like objects.
 They don't have any of the array methods and are not iterable.
 A best practice is converting them to arrays before use, so the compiler can optimize them.
 
-Array.from(arguments) is the best way to do this.
+Array.from(arguments) is one way to convert them to arrays.
+
+Another way is using the spread operator: [...args]
+This is the preferred way, as it is more readable and concise.
+It makes a copy of the arguments with the spread operator, and then converts them to an array.
 
 */
