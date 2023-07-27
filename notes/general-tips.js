ddd43b03a5809c328c9147eee887c3beecbafd3f
CodeConnects <codeconnects@gmail.com>
2023-07-27 10:17:52 -0400

    note on running nested functions IIFE style

---
+++ notes/general-tips.js
@@ -0,0 +1,6 @@
+// () after a variable or function name, just means to run it immediately
+// This is called an IIFE (Immediately Invoked Function Expression)
+// So if connect more than one set of braces, it will run nested functions immediately:
+netsedFunction()()();
+
+
