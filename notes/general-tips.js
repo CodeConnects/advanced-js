bf12cf93a734722581e5f9a9615b421cef5ed634
CodeConnects <codeconnects@gmail.com>
2023-07-27 10:40:09 -0400

    add info on global variable leakage and strict mode

--- notes/general-tips.js
+++ notes/general-tips.js
@@ -3,4 +3,16 @@
 // So if connect more than one set of braces, it will run nested functions immediately:
 netsedFunction()()();
 
+// undeclared variables will attach themselves to the global object
+// this is bad practice, as it can cause bugs, and can be avoided by using strict mode
+// 'use strict' at the top of a file or function will prevent this from happening
+// it will also prevent you from using reserved words as variable names
+'use strict'
+const badFunction = () => {
+  badVar = 'bad';
+}
+badFunction();
+console.log(badVar); // ReferenceError: badVar is not defined
+// this is good practice, as it will prevent bugs and make your code more secure
+
 
